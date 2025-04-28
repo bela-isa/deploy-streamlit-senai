804faf41c1a0c49b02132fa7e887bd1505349e71
Isabela Neves <93232499+gotiquinha@users.noreply.github.com>
2025-04-28 09:43:22 -0300

    Update 0_CHAT.py

--- frontend/0_CHAT.py
+++ frontend/0_CHAT.py
@@ -4,7 +4,7 @@ import requests
 import json
 from datetime import datetime
 
-API_URL = "https://deploy-streamlit-senai.onrender.com/backend"
+API_URL = "https://deploy-streamlit-senai.onrender.com"
 
 # Configuração da página
 st.set_page_config(
